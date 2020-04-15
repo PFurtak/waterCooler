9ad5e4a1d64edc9d81c0ed82598e4587718b7651
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-15 11:03:09 -0400

    Created components for adding and editing logs and techs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import 'materialize-css/dist/css/materialize.min.css';
 import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import EditLogModal from './components/logs/EditLogModal';
+import TechListModal from './components/techs/TechListModal';
 import Logs from './components/logs/Logs';
 
 const App = () => {
@@ -18,6 +21,9 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <AddTechModal />
+        <EditLogModal />
+        <TechListModal />
         <Logs />
       </div>
     </div>
