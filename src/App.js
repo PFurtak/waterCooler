b5807a1fb93cf1bcb3552e4f02737567e03bbc80
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-15 10:14:20 -0400

    Created Add Log Modal component, created add button component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import 'materialize-css/dist/css/materialize.min.css';
 import SearchBar from './components/layout/SearchBar';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import Logs from './components/logs/Logs';
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
     <div className='App'>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </div>
